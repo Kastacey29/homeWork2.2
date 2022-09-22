12f461074f855fc2fdc7792c4fc0a1cb8ad32fe4
Stacey29 <Kastacey@yandex.ru>
2022-09-22 21:13:42 +0300

    первый коммит

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    int yearOfBirth;
+    String name;
+    String town;
+    String position;
+
+    public Human(int yearOfBirth, String name, String town, String position) {
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.town = town;
+        this.position = position;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human maxim = new Human(1987, "Maxim", "Minsk", "brand-manager");
+        Human anya = new Human(1993, "Anna", "Moskow", "methodologistOfEducationalPrograms");
+        Human katya = new Human(1994, "Katya", "Kaliningrad", "productManager");
+        Human artem = new Human(1995, "Artem", "Moskow", "directorOfBusinessDevelopment");
+        System.out.println("Привет! Меня зовут " + maxim.name + ". Я из города " + maxim.town +
+                ". Я родился в " + maxim.yearOfBirth + " году. Я работаю на должности " + maxim.position + ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + anya.name + ". Я из города " + anya.town +
+                ". Я родилась в " + anya.yearOfBirth + " году. Я работаю на должности " + anya.position + ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + katya.name + ". Я из города " + katya.town +
+                ". Я родилась в " + katya.yearOfBirth + " году. Я работаю на должности " + katya.position + ".  Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + artem.name + ". Я из города " + artem.town +
+                ". Я родился в " + artem.yearOfBirth + " году. Я работаю на должности " + artem.position + ".  Будем знакомы!");
     }
 }
